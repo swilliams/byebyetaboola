cd611b482b5562b867eb69e97b8a79b363bbe8d0
Scott Williams <scott@swilliams.me>
2014-10-28 14:57:10 -0700

    first

---
+++ sources/content_script.js
@@ -0,0 +1,19 @@
+function getTaboolaElements() {
+  return document.querySelectorAll('[id^=taboola]');
+}
+
+function deleteTaboolaElements(taboolaElements) {
+  for (var idx = 0; idx < taboolaElements.length; idx += 1) {
+    var el = taboolaElements[idx];
+    el.remove();
+  }
+}
+
+function goTime() {
+  var taboolaElements = getTaboolaElements();
+  console.log(taboolaElements);
+  deleteTaboolaElements(taboolaElements);
+}
+
+window.onload = goTime;
+
